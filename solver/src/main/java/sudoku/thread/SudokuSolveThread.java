efcc6b6ee1257cb9e4306ce84d3e928915dfa4fc
Cole Gannaway <43042842+cole-gannaway@users.noreply.github.com>
2020-02-07 00:35:03 -0500

    Solved Easy 4by4 puzzle

--- solver/src/main/java/sudoku/thread/SudokuSolveThread.java
+++ solver/src/main/java/sudoku/thread/SudokuSolveThread.java
@@ -1,9 +1,9 @@
 package sudoku.thread;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 import sudoku.elements.SudokuCellDataBase;
 import sudoku.elements.SudokuCoordinate;
@@ -13,6 +13,8 @@ import sudoku.solving.utils.SudokuSolvingUtils;
 public class SudokuSolveThread implements Callable<Boolean> {
 	List<SudokuCoordinate> coords;
 	SudokuCellDataBase db;
+	long startTime = System.currentTimeMillis();
+	long printInterval = TimeUnit.SECONDS.toMillis(1);
 
 	public SudokuSolveThread(SudokuCellDataBase db, List<SudokuCoordinate> coords) {
 		this.coords = coords;
@@ -20,14 +22,17 @@ public class SudokuSolveThread implements Callable<Boolean> {
 	}
 
 	@Override
-	public Boolean call() throws Exception {
+	public Boolean call() {
 		Boolean retVal = false;
+		long lastPrintTime = startTime;
 		while (coords.size() != 0) {
 			ListIterator<SudokuCoordinate> it = coords.listIterator();
 			while (it.hasNext()) {
 				SudokuCoordinate coordinate = it.next();
-				List<ESudokuSolveStep> steps = Arrays.asList(ESudokuSolveStep.values());
-				for (ESudokuSolveStep step : steps) {
+				boolean isSolved = false;
+				ESudokuSolveStep[] values = ESudokuSolveStep.values();
+				for (int i = 0; i < values.length && isSolved == false; i++) {
+					ESudokuSolveStep step = values[i];
 					switch (step) {
 					case SETCANDIDATES:
 						SudokuSolvingUtils.setCandidates(db, coordinate);
@@ -36,14 +41,25 @@ public class SudokuSolveThread implements Callable<Boolean> {
 						String uniqueCandidate = SudokuSolvingUtils.hasUniqueCandidate(db, coordinate);
 						if (uniqueCandidate != null) {
 							db.solveCell(coordinate, uniqueCandidate);
+							isSolved = true;
 						}
 						break;
 					default:
 						break;
+
+					}
+				}
+				if (isSolved) {
+					System.out.println("removing " + coordinate + ", " + coords.size() + " left to go.");
+					it.remove();
+				} else {
+					long currentTimeMillis = System.currentTimeMillis();
+					if (currentTimeMillis - lastPrintTime > printInterval) {
+						System.out.println(coords.size() + " left to go.");
+						lastPrintTime = currentTimeMillis;
 					}
 				}
 			}
-
 		}
 		retVal = true;
 		return retVal;
--- solver/src/test/java/sudoku/thread/SudokuThreadManagerTest.java
+++ solver/src/test/java/sudoku/thread/SudokuThreadManagerTest.java
@@ -1,22 +1,46 @@
 package sudoku.thread;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import sudoku.common.test.utils.CommonTestUtils;
 import sudoku.elements.SudokuCellDataBase;
+import sudoku.elements.SudokuCellDataBaseBuilder;
+import sudoku.enums.EBoardType;
+import sudoku.parsing.CSVParser;
 
 public class SudokuThreadManagerTest {
 
 	@Test
-	public void test() throws ExecutionException, TimeoutException {
-		int numOfThreads = 3;
+	@Ignore
+	public void testTimeout() throws ExecutionException, TimeoutException {
+		int numOfThreads = 1;
 		SudokuCellDataBase db = CommonTestUtils.createFakeSudokuDataBase(9);
 		SudokuThreadManager threadManager = new SudokuThreadManager(db, numOfThreads);
 		threadManager.solve(2, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testSolveSingleThread() throws IOException {
+		// set up
+		File testFile = CommonTestUtils.getTestFile("4by4/Easy.csv");
+		List<String[]> fields = CSVParser.parseFile(testFile);
+		SudokuCellDataBase dataBase = SudokuCellDataBaseBuilder.buildDataBase(fields, EBoardType.SUDOKU);
+
+		int numOfThreads = 1;
+		SudokuThreadManager threadManager = new SudokuThreadManager(dataBase, numOfThreads);
+		threadManager.solve(10, TimeUnit.SECONDS);
+		
+		String htmlFileName = "after.html";
+		CommonTestUtils.saveHTMLFileAsOutput(htmlFileName, dataBase.toHTML());
+		CommonTestUtils.openHTMLFileInBrowser(htmlFileName);
 	}
 
 }
