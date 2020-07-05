335ad28bd3268b760485721f785263a2407a9a70
Cole Gannaway <43042842+cole-gannaway@users.noreply.github.com>
2020-07-05 18:46:40 -0400

    Added AWS Lambda JSON response

--- solver/src/main/java/sudoku/app/aws/lambda/LambdaHandler.java
+++ solver/src/main/java/sudoku/app/aws/lambda/LambdaHandler.java
@@ -6,12 +6,15 @@ import java.util.concurrent.TimeUnit;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import sudoku.elements.SudokuCellDataBase;
 import sudoku.elements.SudokuCellDataBaseBuilder;
 import sudoku.parsing.JSONBoardObject;
+import sudoku.parsing.JSONResponse;
 import sudoku.thread.SudokuThreadManager;;
 
 public class LambdaHandler implements RequestHandler<JSONBoardObject, String> {
@@ -25,13 +28,24 @@ public class LambdaHandler implements RequestHandler<JSONBoardObject, String> {
 		logger.log("CONTEXT: " + gson.toJson(context));
 		// process event
 		logger.log("EVENT: " + gson.toJson(request));
+		// get parameters
 		List<List<String>> board = request.getRows();
 		List<String> possibleCandidateValues = request.getPossibleValues();
+		// solve
 		SudokuCellDataBase db = SudokuCellDataBaseBuilder.buildDataBase(board, possibleCandidateValues);
 		SudokuThreadManager manager = new SudokuThreadManager(db, 1);
-		manager.solve(20, TimeUnit.SECONDS);
-		List<List<String>> result = db.toCSV();
-		return result.toString();
+		manager.solve(40, TimeUnit.SECONDS);
+		List<List<String>> resultingBoard = db.toCSV();
+		// write out response
+		JSONResponse response = new JSONResponse("Success", resultingBoard);
+		ObjectMapper mapper = new ObjectMapper();
+		String retVal = null;
+		try {
+			retVal = mapper.writeValueAsString(response);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return retVal.toString();
 	}
 
 }
