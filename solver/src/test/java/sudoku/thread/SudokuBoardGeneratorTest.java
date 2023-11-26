d2bac0384bac80abf68594f0586059d635a807fe
Cole Gannaway <43042842+cole-gannaway@users.noreply.github.com>
2023-11-25 23:31:00 -0600

    renamed

--- solver/src/test/java/sudoku/thread/SudokuBoardGeneratorTest.java
+++ solver/src/test/java/sudoku/thread/SudokuBoardGeneratorTest.java
@@ -16,7 +16,7 @@ import sudoku.elements.SudokuCellDataBase;
 import sudoku.elements.SudokuCellDataBaseBuilder;
 import sudoku.elements.SudokuCoordinate;
 
-public class SudokuBoardGenerator {
+public class SudokuBoardGeneratorTest {
 	List<String> possibleCandidates = Arrays.asList("1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P");
 //	List<String> possibleCandidates = Arrays.asList("1","2","3","4","5","6","7","8","9");
 //	List<String> possibleCandidates = Arrays.asList("1","2","3","4");
